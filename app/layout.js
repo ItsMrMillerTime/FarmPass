452a81a4067a650ccc6d2602769eef26e5903f7b
ItsMrMillerTime <miller.robert@hotmail.com>
2025-05-28 23:14:14 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,16 @@
-// app/layout.js
 import './globals.css'
 import NavBar from '../components/NavBar'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
-      <body className="pt-16">
-        <NavBar />
-        {children}
+      <head/>
+      <body>
+        <NavBar/>
+        {/* push all page content below the fixed 80px nav */}
+        <main className="mt-20">
+          {children}
+        </main>
       </body>
     </html>
   )
