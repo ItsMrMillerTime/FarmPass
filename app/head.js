0725f9afca4caf259d38e2747709557ad4b719a0
ItsMrMillerTime <miller.robert@hotmail.com>
2025-05-28 22:33:15 -0400

    Create head.js

---
+++ app/head.js
@@ -0,0 +1,11 @@
+// app/head.js
+export default function Head() {
+  return (
+    <>
+      <title>Farm Pass</title>
+      <meta name="description" content="One pass to every fun farm on Long Island." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
