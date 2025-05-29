b4e786102b1319c4bdd85bb34e3f1799310db2aa
ItsMrMillerTime <miller.robert@hotmail.com>
2025-05-28 23:44:44 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
+// tailwind.config.js
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', 
+    './pages/**/*.{js,ts,jsx,tsx}',       // in case you add pages/
+    './components/**/*.{js,ts,jsx,tsx}'   // ← ADD THIS LINE
+  ],
   theme: {
     extend: {},
   },
