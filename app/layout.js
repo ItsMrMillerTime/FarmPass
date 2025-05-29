1a41f3270c3b3043d44fefae195a400e0153741c
ItsMrMillerTime <miller.robert@hotmail.com>
2025-05-28 23:27:10 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,13 @@ import NavBar from '../components/NavBar'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head/>
+      <head />
       <body>
-        <NavBar/>
-        {/* push all page content below the fixed 80px nav */}
-        <main className="mt-20">
-          {children}
-        </main>
+        {/* Fixed navbar is 6rem tall (h-24) */}
+        <NavBar />
+
+        {/* Push all page content down by exactly 6rem */}
+        <main className="mt-24">{children}</main>
       </body>
     </html>
   )
