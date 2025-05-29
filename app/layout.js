35ae1d67e026c95d172d4e7434ecf455f9837004
ItsMrMillerTime <miller.robert@hotmail.com>
2025-05-28 22:20:02 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,15 @@
-import './globals.css';
+// app/layout.js
+import './globals.css'
+import NavBar from '../components/NavBar'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className="pt-16">
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
